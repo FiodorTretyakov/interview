a0b4564015b6e0e4fac61e347dbc6cc86beacd97
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-24 11:51:40 +0100

    Update timeDictionary.js
    init

--- timeDictionary.js
+++ timeDictionary.js
@@ -1,1 +1,24 @@
-
+class timeDictionary {
+  constructor() {
+    this.dict = {};
+  }
+  
+  change(key, value) {
+    if (!key) throw new "ArgumentOutOfRangeException";
+    
+    if (!dict[key]) {
+      dict[key] = {
+        "timestamp": 0,
+        "values": {
+          "0": value
+        }
+      };
+      return;
+    }
+    
+    if (dict[key].values[dict[key].timestamp] != value) {
+      dict[key].timestamp++;
+      dict[key][dict[key].timestamp] = value;
+    }
+  }
+}

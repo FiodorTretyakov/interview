e0dd85ee83e4a95626b527682c5455ab6ed7547e
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-24 13:34:38 +0100

    Update timeDictionary.js

--- timeDictionary.js
+++ timeDictionary.js
@@ -2,41 +2,42 @@ class timeDictionary {
   constructor() {
     this.dict = {};
     this.validateKey = function(key) {
+
        if (!key || !key.length) throw new "ArgumentOutOfRangeException";
     }
   }
   
   put(key, value) {
     this.validateKey(key);
-    
     if (!this.dict[key]) {
       this.dict[key] = {
         "timestamp": 0,
         "values": {
-          "0": value
+          0: value
         }
       };
       return;
     }
     
     if (this.dict[key].values[this.dict[key].timestamp] != value) {
-      this.dict[key].timestamp++;
-      this.dict[key][this.dict[key].timestamp] = value;
+      this.dict[key].timestamp = this.dict[key].timestamp + 1;
+      this.dict[key].values[this.dict[key].timestamp] = value;
     }
   }
   
-  get(key, time) {
+  get(key, timestamp) {
     this.validateKey(key);
     
     if (!this.dict[key] || timestamp > this.dict[key].timestamp) throw new "ArgumentOutOfRangeException";
+
     return this.dict[key].values[this.dict[key].timestamp];
   }
 }
 
 let td = new timeDictionary();
-td.put("a", 1)
-console.log(td.get("a", 0))
-td.put("a", 2)
-console.log(td.get("a", 1))
-td.put("a", 2)
-console.log(td.get("a", 2))
+td.put("a", 1);
+console.log(td.get("a", 0));
+td.put("a", 2);
+console.log(td.get("a", 1));
+td.put("a", 2);
+console.log(td.get("a", 1));

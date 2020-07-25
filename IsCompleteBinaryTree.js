11ce7189dd5998c242062ae2c3b951ad9feefe7a
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-26 00:17:01 +0100

    Update IsCompleteBinaryTree.js

--- IsCompleteBinaryTree.js
+++ IsCompleteBinaryTree.js
@@ -1,1 +1,34 @@
+function isComplete(node) {
+  let lastLevel = -1;
+  
+  let q = [{"node": node, "level": 0}];
+  
+  while (q.length) {
+    let e = q.shift();
+    
+    if (e.node.left) {
+      q.push({"node": e.node.left, "level": e.level + 1});
+    } else if (lastLevel === -1){
+      lastLevel = e.level;
+    } else if (lastLevel !== e.level) {
+      return false;
+    }
+    
+    if (e.node.right) {
+      if (!e.node.left) {
+        return false;
+      }
+      q.push({"node": e.node.right, "level": e.level + 1});
+    } else if (lastLevel === -1) {
+      lastLevel = e.level + 1;
+    } else if (e.node.left && lastLevel !== e.level + 1) {
+      return false;
+    }
+  }
+  
+  return true;
+}
 
+
+let node = {"left": {"left": {}}, "right": {"left": {}}};
+console.log(isComplete(node));

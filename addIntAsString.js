aa1daa7774957a85125a0bb51db917d96b0abfa7
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-26 20:19:22 +0100

    Update addIntAsString.js

--- addIntAsString.js
+++ addIntAsString.js
@@ -2,21 +2,24 @@ function add(a, b){
   let tail = 0;
   let result = 0;
   let i = 0;
-  while (a.length || b.length) {
+  let mult = 1;
+  while (a.length > i || b.length > i) {
     let sum = tail;
     tail = 0;
     if (a.length > i) {
-      sum += a[a.length - i];
+      sum += parseInt(a[a.length - i - 1]);
     }
     if (b.length > i) {
-      sum += b[b.length - i];
+      sum += parseInt(b[b.length - i - 1]);
     }
     if (sum >= 10) {
       tail = 1;
       sum -= 10;
     }
     
-    result += 10 * (i + 1);
+    result += sum * mult;
+    i++;
+    mult *= 10;
   }
   
   return result;

69257f3039a31404ca49f94441feab5851463d37
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-26 08:57:46 +0100

    Update lru.js

--- lru.js
+++ lru.js
@@ -1,1 +1,15 @@
-
+class lru {
+  constructor() {
+    this.hash = {};
+    this.list = {);
+  }
+    
+  put(e) {
+    if (!this.hash[e])) {
+      let listItem = {
+        "next": this.list
+      }
+      this.hash[e] = 
+    }
+  }
+}

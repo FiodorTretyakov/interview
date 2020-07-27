0d5b7c8d90a73350c5e72069e98957f0c11bb55a
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-27 10:58:42 +0100

    Update closestPoints.js

--- closestPoints.js
+++ closestPoints.js
@@ -1,8 +1,10 @@
 function getClosest(n, k, p) {
   let result = [];
   for (let i = 0; i < n.length; i++) {
-    result.push(Math.pow(n[i].x - p.x, 2) + Math.pow(n[i].y - p.y), 2);
+    result.push({"number": i, "value": Math.pow(n[i].x - p.x, 2) + Math.pow(n[i].y - p.y), 2);
   }
 
-  return result.sort().slice(0, k);
+  return result.sort(function (a,b) {
+    return a.value - b.value;
+  }).slice(0, k).map(e => n[e.number]);
 }

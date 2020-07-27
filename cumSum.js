c21808ecbcfb45e91691103671f3b95008d0d270
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-27 11:33:01 +0100

    Update cumSum.js

--- cumSum.js
+++ cumSum.js
@@ -1,1 +1,15 @@
+function cumSum(a) {
+  let result = [];
+  
+  for (let i = 0; i < a.length; i++) {
+    if (i === 0) {
+      result.push(a[i]);
+    } else {
+      result.push(a[i] + result[i - 1]);
+    }
+  }
 
+  return result;
+}
+
+console.log(cumSum([13, 34, 32, 232, 1, -2]));

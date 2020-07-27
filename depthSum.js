f45dcd0d86eb027a8a976aac2caf55a8c683a142
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-07-27 11:53:16 +0100

    Update depthSum.js

--- depthSum.js
+++ depthSum.js
@@ -1,1 +1,14 @@
+function depthSum(n, m) {
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (!n[i].length) {
+      sum += n[i] * m;
+    } else {
+      sum += depthSum(n[i], m + 1);
+    }
+  }
+  
+  return sum;
+}
 
+console.log(depthSum([1, [2, 3, 5], 3, [[3], 3, 4], [5]], 1));

43ab02e4b89b91c7b105893925ba03ed8f5d9fa2
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-08-04 12:23:01 +0100

    Update almostPalindrome.js

--- almostPalindrome.js
+++ almostPalindrome.js
@@ -1,1 +1,30 @@
+function ap(n) {
+  let modStart = 0;
+  let modEnd = 0;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    let es = n[i + modStart];
+    let ee = n[n.length - 1 - i - modEnd]
+    if (es === ' ') {
+      modStart++;
+      i--;
+      continue;
+    }
+    if (ee === ' ') {
+      modEnd++;
+      i--;
+      continue;
+    }
+    
+    if (es != ee) {
+      return false;
+    }
+  }
+  
+  return true;
+}
 
+console.log(ap("a"));
+console.log(ap("ab"));
+console.log(ap("aba"));
+console.log(ap("abba"));
+console.log(ap("abc ba"));

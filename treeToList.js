785ba5017afd6c778653b00f1cf4febba8cf529d
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-08-04 12:49:40 +0100

    Update treeToList.js

--- treeToList.js
+++ treeToList.js
@@ -1,1 +1,24 @@
+function toList(node) {
+  if (node.left) {
+    let n = toList(node.left);
+    while (n.next) {
+      n = n.next;
+    }
+    n.next = node;
+    node.prev = n;
+  } else {
+    return node;
+  }
+  if (node.right) {
+    let n = toList(node.right);
+    while (n.prev) {
+      n = n.prev;
+    }
+    node.next = n;
+    n.prev = node;
+  } else {
+    return node;
+  }
 
+  return node;
+}

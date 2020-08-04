2a3c21b79bb20b781792f99f955c7a4dab87d34f
Ted Tratsiakou <Fiodor.Tretyakov@gmail.com>
2020-08-04 12:32:30 +0100

    Update brackets.js

--- brackets.js
+++ brackets.js
@@ -1,1 +1,22 @@
+function brackets(n) {
+  let pairs = {")": "(", "]": "[", "}": "{"};
+  
+  let stack = [];
+  while (n.length) {
+    let e = n.shift();
+    if (stack.length && pairs[e] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(e);
+    }
+  }
 
+  return stack.reverse().map(e => pairs[e]);
+}
+
+console.log(brackets(""));
+console.log(brackets("("));
+console.log(brackets("()"));
+console.log(brackets("()[]"));
+console.log(brackets("(())"));
+console.log(brackets("(([)"));
